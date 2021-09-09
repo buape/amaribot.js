7486558ba4924137618d525e1d926fa2b12cdd3a
Shadow <thebotoftheshadow@gmail.com>
2021-09-09 16:18:56 -0500

    queue framework

--- src/RequestHandler.js
+++ src/RequestHandler.js
@@ -1,11 +1,14 @@
 const axios = require("axios")
 const { RatelimitError, APIError } = require("./errors")
+const { AsyncQueue } = require("@sapphire/async-queue")
+const queue = new AsyncQueue()
 class RequestHandler {
     constructor(client) {
         this._client = client
     }
 
-    request(endpoint, query = {}, method, _attempts = 0) {
+    async request(endpoint, query = {}, method, _attempts = 0) {
+        await queue.wait()
         return new Promise((resolve, reject) => {
             const options = {
                 validateStatus: null,
@@ -22,22 +25,25 @@ class RequestHandler {
             }
 
             if (this._client.debug) console.debug(`Sending request to ${options.url}\nMethod:\n  ${options.method}\nParams:\n  ${query}`)
+            try {
+                axios.request(options).then((res) => {
+                    //  Increase the number of attempts
+                    ++_attempts
 
-            axios.request(options).then((res) => {
-                //  Increase the number of attempts
-                ++_attempts
-
-                if (res.status >= 200 && res.status < 300) {
-                    resolve(res.data)
-                    if (this._client.debug) console.debug("Success: \n", res.data)
-                } else if (res.status === 429) {
-                    if (this._client.debug) console.debug("Ratelimited: \n", res) 
-                    reject(new RatelimitError(res))
-                } else {
-                    if (this._client.debug) console.debug("API Error: \n", res) 
-                    reject(new APIError(res))
-                } 
-            })
+                    if (res.status >= 200 && res.status < 300) {
+                        resolve(res.data)
+                        if (this._client.debug) console.debug("Success: \n", res.data)
+                    } else if (res.status === 429) {
+                        if (this._client.debug) console.debug("Ratelimited: \n", res)
+                        reject(new RatelimitError(res))
+                    } else {
+                        if (this._client.debug) console.debug("API Error: \n", res)
+                        reject(new APIError(res))
+                    }
+                })
+            } finally {
+                queue.shift()
+            }
         })
     }
 }
