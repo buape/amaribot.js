978f1252a050fcc8dc5beceffecebce2bc613f79
Shadow <thebotoftheshadow@gmail.com>
2021-10-23 20:37:40 -0500

    Small changes in the API

--- src/structures/Rewards.js
+++ src/structures/Rewards.js
@@ -1,5 +1,4 @@
 /** The rewards from a guild's config
- * @property {string} id - The guild's Discord ID
  * @property {number} count - The count of rewards the guild has
  * @property {Map<number, string>} roles A Map of the role rewards in the guild. The key is the level, and the value is the role ID
  */
@@ -10,10 +9,9 @@ class Rewards {
      * @description This is set of the Rewards from a guild
      */
     constructor(inputData = {}) {
-        this.id = inputData.id
         this.count = inputData.count
         this.roles = inputData.data.reduce(
-            (map, v) => map.set(v.level, v.roleId),
+            (map, v) => map.set(v.level, v.roleID),
             new Map()
         );
 
