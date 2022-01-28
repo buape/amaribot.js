27d4b1f63f085b11939fa56047d793304f838e6f
thewilloftheshadow <william.shadow@outlook.com>
2022-01-28 04:46:16 +0000

    Add documentation for error structures

--- src/errors/APIError.js
+++ src/errors/APIError.js
@@ -1,3 +1,10 @@
+/**
+ * @extends Error
+ * @property {string} name The type of error (APIError)
+ * @property {number} status HTTP status of the error
+ * @property {string} message The message of this error
+ */
+
 class APIError extends Error {
     constructor(response) {
         super();
--- src/errors/RatelimitError.js
+++ src/errors/RatelimitError.js
@@ -1,5 +1,13 @@
 const ms = require("ms")
 
+/**
+ * @extends Error
+ * @property {string} name The type of error (RatelimitError)
+ * @property {number} status HTTP status of the error
+ * @property {number} remaining The time remaining for your ratelimit to expire in milliseconds
+ * @property {string} message The message of this error
+ */
+
 class RatelimitError extends Error {
     constructor(response) {
         super();
